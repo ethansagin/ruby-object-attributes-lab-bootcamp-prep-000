7654a9aafde8112ecbf3c4f2663f9d1807fd69dc
Ethan <ethansagin@gmail.com>
2019-02-27 20:42:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
   def name
@@ -9,6 +10,14 @@ class Dog
   
   def name=(new_name)
     @name = new_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
   end
   
 end
